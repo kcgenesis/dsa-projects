4eff0f30680a42996232a0c3ae1ad349a6dea5da
Kirk Cameron <kirk.t.cameron@gmail.com>
2016-03-26 14:48:05 -0400

    spellcheck.cpp split function modified, lowercase conversion fixed

--- hash-table/spellcheck.cpp
+++ hash-table/spellcheck.cpp
@@ -9,15 +9,44 @@
 using namespace std;
 
 
-//fix delimiter
-vector<string> &split(const string &s, char delim, vector<string> &elems) {
-	stringstream ss(s);
+//uses an ascii range to denote valid characters
+vector<string> &split(const string &s, vector<int*> &range, vector<string> &elems) {
+	string r = s;
+	char delim = ' ';
+	int* ptr;
+	bool valid;
+	for(int i=0;i<r.size();i++){
+		valid = true;
+		for(int j=0;j<range.size();j++){
+			ptr = range[j];
+			if ((ptr[0] <= (int)r[i])&&((int)r[i] <= ptr[1])) {
+				valid = true;
+				break;
+			}
+			else{
+				valid = false;
+			}
+		}
+		if (!valid) {
+			r[i] = delim;
+		}
+	}
+	stringstream ss(r);
 	string item;
 	while (getline(ss, item, delim)) {
-	    elems.push_back(item);
+		if(item.size()!=0){
+			elems.push_back(item);
+		}
 	}
 	return elems;
 }
+
+
+
+
+
+
+
 
 int main(){
 	hashTable d;
@@ -42,27 +71,46 @@ int main(){
 	t = clock();
 	if(file2.is_open()&&file3.is_open()){
 		vector<string> words;
-		//locale loc;
+		vector<int*> valid;
+		
+		//valid ascii ranges
+		int range[5][2];
+		range[0][0] = 'A';
+		range[0][1] = 'Z';
+		valid.push_back(range[0]);
+		range[1][0] = 'a';
+		range[1][1] = 'z';
+		valid.push_back(range[1]);
+		range[2][0] = '0';
+		range[2][1] = '9';
+		valid.push_back(range[2]);
+		range[3][0] = '-';
+		range[3][1] = '-';
+		valid.push_back(range[3]);
+		range[4][0] = '\'';
+		range[4][1] = '\'';
+		valid.push_back(range[4]);
+
 		string line;
 		int n = 1;
 		while(getline(file2,line)){
-			words = split(line,' ',words);
+			words = split(line,valid,words);
 			for(int i=0;i<words.size();i++){
+				for(int j=0;j<words[i].size();j++){
+						if(('A'<=words[i][j])&&(words[i][j]<='Z')){
+							words[i][j] = words[i][j] + ('a' - 'A');
+						}
+				}
 				if(words[i].size()>20){
 					file3<<"Long word at line "<< n <<", ";
 					file3<<"starts: "<<words[i].substr(0,20)<<endl;
 				}
 				else{
-					for(int j=0;j<words[i].size();j++){
-						//words[i][j] = tolower(words[i][j],loc);
-						words[i][j] = tolower(words[i][j]);
-					}
 					if(!d.contains(words[i])){
 						file3<<"Unknown word at line "<< n <<": ";
 						file3<< words[i] <<endl;
 					}
 				}
-				
 			}
 			n++;
 			words.erase(words.begin(), words.end());
